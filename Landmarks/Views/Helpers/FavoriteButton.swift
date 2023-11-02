6846802ed3d82532c97a3ac5badc9003b7db6595
Andy Kruch <avk.16@yandex.ru>
2023-11-02 13:16:34 +0300

    Added States model model Data UI favorites feature

---
+++ Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Andy Kruch on 02.11.23.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star") .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow: .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
