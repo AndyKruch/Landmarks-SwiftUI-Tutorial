89092ccae20752f81bd8febd5b322b41cf7df7d5
Andy Kruch <avk.16@yandex.ru>
2023-11-02 17:17:25 +0300

    Added Navigation Screen

---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Andy Kruch on 02.11.23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
